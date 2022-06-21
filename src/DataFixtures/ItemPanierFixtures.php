54e7f4ac71df564b6bfd7f7dfd48821c1dea3b11
Vincent Creff <creff.vincent@gmail.com>
2022-06-21 16:55:22 +0200

    panier + item panier / v.0.1

---
+++ src/DataFixtures/ItemPanierFixtures.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ItemPanierFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+       //TO DO
+    }
+}
